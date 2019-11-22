b4a09e0a8cc8b6a33722ca7d8c9a249a21e56b7b
Thibaut76600 <lavisse.thibaut@gmail.com>
2019-11-22 14:12:46 +0100

    Exercice 9 - JS

---
+++ assets/js/displayVar2.js
@@ -0,0 +1,3 @@
+function displayVar2() {
+    alert(lastName2 + '\n' + firstName2 + '\n' + city2);
+}
---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+function affichage() {
+    var write = document.getElementById('write').value;
+    alert(write);
+}
